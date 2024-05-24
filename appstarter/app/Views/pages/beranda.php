5091b691e9087b7dc53762a73c3cf8fca2271679
daffamaul <dffmln2004@gmail.com>
2024-05-24 14:32:50 +0700

    feat(views): add jumbotron for beranda

--- appstarter/app/Views/pages/beranda.php
+++ appstarter/app/Views/pages/beranda.php
@@ -1,5 +1,17 @@
 <?= $this->extend('pages/template') ?>
 
 <?= $this->section('content') ?>
-<h2>Beranda</h2>
+<div class="jumbotron mt-3">
+    <div class="jumbotron-desc">
+        <h2>Selamat Datang!</h2>
+        <p>Kami memberikan solusi kepada anda sewa ruko dengan mudah menggunakan jari anda.</p>
+        <div class="btn-cta">
+            <a href="<?= base_url('/daftar-ruko') ?>">Lihat Ruko</a>
+            <a href="">Lokasi</a>
+        </div>
+    </div>
+    <div class="jumbotron-img">
+        <img src="<?= base_url('/images/1.jpg') ?>" alt="">
+    </div>
+</div>
 <?= $this->endSection() ?>
