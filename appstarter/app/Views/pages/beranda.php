0a1b16836621719ce8aabe9a3fc8b324ff0b46a2
daffamaul <dffmln2004@gmail.com>
2024-05-25 12:02:41 +0700

    feat(views): add profile and testimoni

--- appstarter/app/Views/pages/beranda.php
+++ appstarter/app/Views/pages/beranda.php
@@ -14,4 +14,72 @@
         <img src="<?= base_url('/images/1.jpg') ?>" alt="">
     </div>
 </div>
+<div class="profile mt-5">
+    <h3>Tentang Pemilik</h3>
+    <div class="profile-card mt-3">
+        <img src="<?= base_url('/images/person.jpg') ?>" alt="">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error asperiores dolore nemo ad cupiditate tempore quia adipisci maiores, molestias sint odit, autem iure cumque amet quod sed quo. Quidem eum laudantium reiciendis fugit nobis ducimus magni illum deleniti, ipsam quibusdam sint ea soluta. Repudiandae dolor dolores necessitatibus illo animi laboriosam ducimus soluta voluptatibus atque dolorem dolorum, alias nostrum placeat quis nemo voluptate, veritatis, nihil non facilis! Earum, maiores qui necessitatibus dolorem sapiente, sint dolores praesentium magni, quo totam vel libero! In omnis, aliquam, asperiores eligendi architecto molestiae similique enim deserunt vitae, est illo nostrum nemo nulla rem impedit quam aperiam. Tempore tenetur asperiores explicabo dolorum ratione pariatur porro esse eaque quisquam, laborum, ducimus minus cumque alias dolore quidem sunt unde quis autem facere laboriosam, architecto minima labore nulla. Voluptatum, atque explicabo vel ad excepturi consequatur iusto ea odit expedita enim porro ab. Aspernatur soluta temporibus quidem nobis? Debitis, perferendis sit?</p>
+    </div>
+</div>
+<div class="testimoni mt-5">
+    <h3>Kata Mereka</h3>
+    <div class="testimoni-list__card">
+        <div class="testimoni-card mt-3">
+            <div class="testimoni-card__photo">
+                <img src="<?= base_url('/images/person.jpg') ?>" alt="" width="100" height="100">
+                <div class="testimono-card__username__star">
+                    <h6>username</h6>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            </div>
+            <p>"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, cumque dolore maxime velit ipsa corrupti! Quia veniam beatae cum velit porro ullam sint! Fugit, nostrum!"</p>
+        </div>
+        <div class="testimoni-card mt-3">
+            <div class="testimoni-card__photo">
+                <img src="<?= base_url('/images/person.jpg') ?>" alt="" width="100" height="100">
+                <div class="testimono-card__username__star">
+                    <h6>username</h6>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            </div>
+            <p>"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, cumque dolore maxime velit ipsa corrupti! Quia veniam beatae cum velit porro ullam sint! Fugit, nostrum!"</p>
+        </div>
+        <div class="testimoni-card mt-3">
+            <div class="testimoni-card__photo">
+                <img src="<?= base_url('/images/person.jpg') ?>" alt="" width="100" height="100">
+                <div class="testimono-card__username__star">
+                    <h6>username</h6>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            </div>
+            <p>"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, cumque dolore maxime velit ipsa corrupti! Quia veniam beatae cum velit porro ullam sint! Fugit, nostrum!"</p>
+        </div>
+        <div class="testimoni-card mt-3">
+            <div class="testimoni-card__photo">
+                <img src="<?= base_url('/images/person.jpg') ?>" alt="" width="100" height="100">
+                <div class="testimono-card__username__star">
+                    <h6>username</h6>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            </div>
+            <p>"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, cumque dolore maxime velit ipsa corrupti! Quia veniam beatae cum velit porro ullam sint! Fugit, nostrum!"</p>
+        </div>
+    </div>
+</div>
 <?= $this->endSection() ?>
--- appstarter/app/Views/pages/template.php
+++ appstarter/app/Views/pages/template.php
@@ -11,6 +11,9 @@
 
   <!-- My Style -->
   <link rel="stylesheet" href="<?= base_url('/style.css') ?>">
+
+  <!-- Icon -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -21,6 +24,10 @@
 
     <!-- Content -->
     <?= $this->renderSection('content') ?>
+  </div>
+
+  <div class="footer mt-5">
+    <p>Website ini dimiliki oleh Ruko Keisha &copy; 2024</p>
   </div>
 
   <!-- Popper and JS Bootstrap -->
